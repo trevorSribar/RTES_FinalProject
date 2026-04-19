0551a5611f4593f9e5cbf1b76e58e934e8445ea6
Kenny A <58489290+alken2@users.noreply.github.com>
2026-04-19 05:33:11 -0400

    Update UART header with tested version

--- headers/uart.h
+++ headers/uart.h
@@ -1,13 +1,14 @@
 #include "generic.h"
 #include <syslog.h>
-#include <WiringSerial.h>
+#include <wiringSerial.h>
 
 #define BAUD_RATE 115200
 #define BUFFER_SIZE 100
+#define MAX_POLLS 500
 #define TEST_SET 1
 
-//send string <s> over UART over a length of <s_len>
-void uart_send(char *s, int s_len);
+//send string s over UART over a length of s_len with a type of s_type representing unencrypted as 0, encrypted as 1, and laser toggle as 2
+void uart_send(char *s, int s_len, int s_type);
 
 //receive a string over UART
 char *uart_receive();
@@ -15,5 +16,11 @@ char *uart_receive();
 //test set function for the sending Raspberry Pi (TEST_SET=1)
 void sender_test_set();
 
-//test set function for the receiving Raspberry Pi (TEST_SET=0)
+//test set function for the receiving Raspberry Pi (TEST_SET=2)
 void receiver_test_set();
+
+//initialize UART through WiringPi library
+int initialize_uart();
+
+//close UART connection through WiringPi
+void close_uart();

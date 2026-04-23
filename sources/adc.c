a517f579cb8079f6d025a94fd573074f8b0d25b0
Kenny A <58489290+alken2@users.noreply.github.com>
2026-04-22 23:25:40 -0400

    Update adc.c to with working functions

--- sources/adc.c
+++ sources/adc.c
@@ -1,27 +1,32 @@
-ADS1x1x_config_t ads1115_cfg;
-int fd;
-uint16_t calibration = 0;
+#include "adc.h"
+
+struct wiringPiNodeStruct *adc_node;
+int calibration = 0;
 
 void init_ads1115(void)
 {
     // Initialise ADC
-    if (ADS1x1x_init(&ads1115_cfg, ADS1115, DEVICE_ID, MUX_SINGLE_0, PGA_4096) == 0)
+    if (ads1115Setup(&adc_node, PIN_BASE, DEVICE_ID) == 0)
     {
-        printf("Could not initialize ADS1115 - Module and photoresistor cannot be read\n");
+        syslog(LOG_PERROR, "Could not initialize ADS1115 - Photoresistor cannot be read\n");
+        return;
     }
-
-    ADS1x1x_set_data_rate(&ads1115_cfg, DATA_RATE_ADS111x_860);
 }
 
 void calibrate_ads1115(void)
 {
-    calibration = ADS1x1x_read(&ads1115_cfg);
+    calibration = adc_node->analogRead(adc_node, 0);
 }
 
-void capture_ads1115(uint16_t *buffer, int b_len)
+void capture_ads1115(uint16_t **buffer, int b_len)
 {
     for (int i = 0; i < b_len; i++)
     {
-        buffer[i] = ADS1x1x_read(&ads1115_cfg) - calibration;
+        (*buffer)[i] = (uint16_t)adc_node->analogRead(adc_node, 0);
     }
+}
+
+uint16_t read_ads1115(void)
+{
+    return (uint16_t)adc_node->analogRead(adc_node, 0);
 }

3276e4738850ff701f2c376075740d0cec28e4c0
trsr <trsr6750#@colorado.edu>
2026-04-27 23:42:06 -0600

    drastically increased ADC read high value

--- headers/adc.h
+++ headers/adc.h
@@ -7,7 +7,7 @@
 
 #define DEVICE_ID 0x48
 #define PIN_BASE 200
-#define ADC_PHOTOSENSOR_READ_HIGH 400
+#define ADC_PHOTOSENSOR_READ_HIGH 2000
 
 //Initialize the ADS1115 device 
 void init_ads1115(void);
